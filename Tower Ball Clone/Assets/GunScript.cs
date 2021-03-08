6f15ad7e2aa644ef11d3fa44dc7b0cf4650c0eb7
Mertcan <aircadetxy@gmail.com>
2021-03-09 01:48:23 +0300

    Prototype is ready

---
+++ Tower Ball Clone/Assets/GunScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunScript : MonoBehaviour
+{
+    public GameObject ball;
+    public Vector3 ballSpeed;
+
+    bool crRunning;
+    private void Update()
+    {
+        if (GameManagerScript.playerState == GameManagerScript.PlayerState.Playing && Input.GetMouseButton(0))
+        {
+            if (!crRunning)
+            {
+                StartCoroutine(Shoot());
+            }
+
+        }
+    }
+
+    IEnumerator Shoot()
+    {
+        crRunning = true;
+        GameObject ball2 = Instantiate(ball, new Vector3(0, 0, 0), Quaternion.Euler(0, 0, 0));
+        ball2.AddComponent<Rigidbody>();
+        ball2.GetComponent<Rigidbody>().useGravity = false;
+        ball2.GetComponent<Rigidbody>().AddForce(ballSpeed, ForceMode.Impulse);
+
+        yield return new WaitForSeconds(.3f);
+        crRunning = false;
+
+    }
+}
